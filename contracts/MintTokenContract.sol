c80f8da171203bcc8767f5339b61bbc0ee5ac428
kimdg1105 <kimss7334@naver.com>
2022-07-19 15:03:34 +0900

    init: First commit

---
+++ contracts/MintTokenContract.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "./SaleTokenContract.sol";
+
+contract MintTokenContract is ERC721Enumerable {
+    SaleTokenContract public saleTokenContract;
+
+    // constructor
+    constructor() ERC721("Dongguri", "DGR") {}
+
+    // unique tokenId -> tokenTypes
+    mapping(uint256 => uint256) public tokenTypes;
+
+    struct DGRTokenData {
+        uint256 tokenId;
+        uint256 tokenType;
+        uint256 tokenPrice;
+    }
+
+    // functions
+
+    // mint token (return -> tokenId)
+    function mintToken() public returns (uint256) {
+        // unique token id
+        uint256 tokenId = totalSupply() + 1;
+
+        // generate Random tokenType(1~5)
+        uint256 tokenType = (uint256(
+            keccak256(abi.encodePacked(block.timestamp, msg.sender, tokenId))
+        ) % 5) + 1;
+
+        tokenTypes[tokenId] = tokenType;
+
+        _mint(msg.sender, tokenId);
+        return tokenId;
+    }
+
+    function getTokens(address _tokenOwner)
+        public
+        view
+        returns (DGRTokenData[] memory)
+    {
+        uint256 balanceLength = balanceOf(_tokenOwner);
+        require(balanceLength > 0, "No tokens");
+
+        DGRTokenData[] memory tokenDataArray = new DGRTokenData[](
+            balanceLength
+        );
+
+        for (uint256 i = 0; i < balanceLength; i++) {
+            uint256 tokenId = tokenOfOwnerByIndex(_tokenOwner, i);
+            uint256 tokenType = tokenTypes[tokenId];
+            uint256 tokenPrice = saleTokenContract.getTokenPrice(tokenId);
+
+            tokenDataArray[i] = DGRTokenData(tokenId, tokenType, tokenPrice);
+        }
+
+        return tokenDataArray;
+    }
+
+    function setSaleTokenContract(address _saleTokenContractAddress) public {
+        saleTokenContract = SaleTokenContract(_saleTokenContractAddress);
+    }
+
+    function getTokenType(uint256 _tokenId) public view returns (uint256) {
+        return tokenTypes[_tokenId];
+    }
+}
---
+++ contracts/SaleTokenContract.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./MintTokenContract.sol";
+import "./node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract SaleTokenContract {
+    MintTokenContract public mintTokenContract;
+
+    // constructor
+    constructor(address _mintAnimalTokenAddress) {
+        mintTokenContract = MintTokenContract(_mintAnimalTokenAddress);
+    }
+
+    // structs
+    struct DGRTokenData {
+        uint256 tokenId;
+        uint256 tokenType;
+        uint256 tokenPrice;
+    }
+
+    // mappings
+    mapping(uint256 => uint256) public tokenPrices;
+    // arrays
+    uint256[] public onSaleTokenArray;
+
+    function setForSaleToken(uint256 _tokenId, uint256 _price) public {
+        address tokenOwner = mintTokenContract.ownerOf(_tokenId);
+
+        require(tokenOwner == msg.sender, "Caller is not token owner");
+        require(_price > 0, "Price must be greater than 0");
+        require(tokenPrices[_tokenId] == 0, "Token is already for sale");
+        require(
+            mintTokenContract.isApprovedForAll(tokenOwner, address(this)),
+            "Token owner did not approve token"
+        );
+
+        tokenPrices[_tokenId] = _price;
+        onSaleTokenArray.push(_tokenId);
+    }
+
+    function purchaseToken(uint256 _tokenId) public payable {
+        uint256 price = tokenPrices[_tokenId];
+        address tokenOwner = mintTokenContract.ownerOf(_tokenId);
+
+        require(price > 0, "Token is not for sale");
+        require(price <= msg.value, "Insufficient price");
+        require(tokenOwner != msg.sender, "You cannot purchase your own token");
+        tokenPrices[_tokenId] = 0;
+
+        payable(tokenOwner).transfer(msg.value);
+        mintTokenContract.safeTransferFrom(tokenOwner, msg.sender, _tokenId);
+
+        for (uint256 i = 0; i < onSaleTokenArray.length; i++) {
+            if (tokenPrices[onSaleTokenArray[i]] == 0) {
+                removeItemInArray(onSaleTokenArray, i);
+            }
+        }
+    }
+
+    function getOnSaleTokens() public view returns (DGRTokenData[] memory) {
+        uint256 onSaleTokenArrayLength = getOnSaleTokenArrayLength();
+
+        DGRTokenData[] memory onSaleTokenDataArray = new DGRTokenData[](
+            onSaleTokenArrayLength
+        );
+
+        for (uint256 i = 0; i < onSaleTokenArrayLength; i++) {
+            uint256 tokenId = onSaleTokenArray[i];
+            uint256 tokenType = mintTokenContract.getTokenType(tokenId);
+            uint256 tokenPrice = tokenPrices[tokenId];
+
+            onSaleTokenDataArray[i] = DGRTokenData(
+                tokenId,
+                tokenType,
+                tokenPrice
+            );
+        }
+
+        return onSaleTokenDataArray;
+    }
+
+    function getOnSaleTokenArrayLength() public view returns (uint256) {
+        return onSaleTokenArray.length;
+    }
+
+    function getTokenPrice(uint256 _tokenId) public view returns (uint256) {
+        return tokenPrices[_tokenId];
+    }
+
+    // Helper functions
+    function removeItemInArray(uint256[] storage _array, uint256 _index)
+        private
+    {
+        _array[_index] = _array[_array.length - 1];
+        _array.pop();
+    }
+}
